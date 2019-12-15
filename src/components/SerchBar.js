6648fb60d883ef78d5d8ff84dee2126b56acf2d7
Dai <d.k.95.82.8192@icloud.com>
2019-12-15 01:40:53 -0800

    part 4

--- src/components/SerchBar.js
+++ src/components/SerchBar.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-class SerchBar extends React.Component{
+class SerchBar extends React.Component {
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
     render() {
         return <div className="ui segment">
             <form className="ui form">
                 <div className="field">
                     <label>Image Search</label>
-                <input type="text"/>
+                    <input type="text"
+                           onChange={this.onInputChange}
+                    />
                 </div>
             </form>
         </div>
