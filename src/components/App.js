13160eba2ff893e06ca57aa36685521b87a9202b
Dai <d.k.95.82.8192@icloud.com>
2019-12-15 01:25:55 -0800

    part 3

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import SerchBar from "./SerchBar";
 const App = ()=>{
-    return <div>
+    return <div className="ui container" style={{marginTop:'10px'}}>
         <SerchBar />
     </div>;
 };
--- src/components/SerchBar.js
+++ src/components/SerchBar.js
@@ -2,9 +2,12 @@ import React from "react";
 
 class SerchBar extends React.Component{
     render() {
-        return <div>
-            <form >
+        return <div className="ui segment">
+            <form className="ui form">
+                <div className="field">
+                    <label>Image Search</label>
                 <input type="text"/>
+                </div>
             </form>
         </div>
     }
