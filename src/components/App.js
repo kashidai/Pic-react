8230125df106bf83ad0faac3a8c0f838b2776ea7
Dai <d.k.95.82.8192@icloud.com>
2019-12-15 03:19:30 -0800

    add axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import axios from 'axios';
 import SerchBar from "./SerchBar";
-const App = ()=>{
-    return <div className="ui container" style={{marginTop:'10px'}}>
-        <SerchBar />
-    </div>;
-};
+
+class App extends React.Component{
+    onSearchSubmit(term){
+
+    }
+
+    render() {
+    return (
+        <div className="ui container" style={{marginTop:'10px'}}>
+        <SerchBar onSubmit={this.onSearchSubmit} />
+    </div>
+    );
+    }
+}
 
 export default App;
--- src/components/SerchBar.js
+++ src/components/SerchBar.js
@@ -2,17 +2,23 @@ import React from "react";
 
 class SerchBar extends React.Component {
     state = {
-        term:''
+        term: ''
+    }
+
+    onFormSubmit = event=> {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term)
     }
 
     render() {
         return <div className="ui segment">
-            <form className="ui form">
+            <form onSubmit={this.onFormSubmit} className="ui form">
                 <div className="field">
                     <label>Image Search</label>
                     <input type="text"
                            value={this.state.term}
-                           onChange={(e) => this.setState({term: e.target.value.})}
+                           onChange={e => this.setState({term: e.target.value})}
                     />
                 </div>
             </form>
